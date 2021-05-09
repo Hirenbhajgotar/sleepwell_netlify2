bfb6cefe164e9d13145944425b28ae3331ba927e
anilbhajgotar <bhajgotarab@gmail.com>
2021-05-09 17:53:09 +0530

    setup homepage variable in package.json

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import './scss/style.scss';
 
+// deploy key
+// ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDdKK0FgNDjIKyhjNYTzUBOoLu/pZOkMDi0dxZHynaOo2qOiQEHKGVJQTDG+DR9h6vxa3eFCl7RoaoDJFwkHS0qN2HtOeSd4sWsu81AF2+EXzQiupfgFKI55wBNmXgtt9oHTpKK0U5jofQbuBIQ24IDVmNyqgU8yu8xRot4VaCqyk9Pm56uXGo0Oc5aHVYmHAdSvSfqKuMbmnmyAI9E0qS2B5lAS3ltZ05yCZTwOXhrwFcMNZCtMSqtIwSkZifHi+4tl2syb1bgRmjdwOp2nNdcJMZdngV12WjK7p88PvlB9rbpKZ/bZbwH8GRcdb7x+UC8okKKUvw4FB/JZ4KHgip3w1qBWwM9jVuSCwDE7NYOKYjqiS2UFWLP8AsvPZCj5lEih61ohNpb3PM8HsekDo9o3O2zXUQE/rTgt2PXbzUMD2BDZ9yUjCj94oz8c3k8PDFqzSC8cZEiBRC1eIBmUEE8WjNkRh9nfmomUg+xq/+WMzVjDkeePQ+IF29F4OEK7yjA2krEf5y5dlfUuRM2HNlVErERi0+PZvAJ7jjSfTAP4C0pUv/Pzp4zEO65w5+S1pp7+6n49JEUJ2B3965rPCUruEMjQKd8WA5On8CcevIqIuCsiqouJtq1ID/i30jtYInWgX+KjzA72xGsnVlJjkt3EWmSTR6DCi9JwO6r8HcfCw==
+
+// api id
+// 3dc6aeb1-bfd1-4fa2-b157-f6495efce954
+
+// sitename
+// sleepwellnetlify
+
+
+
 const loading = (
   <div className="pt-3 text-center">
     <div className="sk-spinner sk-spinner-pulse"></div>
